cb6e22e8c6c0f7916c58d5ca9503fdb2eb0e5438
seanpden <seanden522@gmail.com>
2025-10-10 15:41:13 -0500

    refactor: 🔨 move library to lib file, add integration tests

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+#[derive(Debug, Clone)]
+pub struct Stock {
+    pub id: String,
+    pub name: String,
+    pub initial_value: f64,
+    pub current_value: f64,
+    pub units: String,
+}
+
+impl Stock {
+    pub fn build(
+        id: &str,
+        name: &str,
+        initial_value: f64,
+        current_value: f64,
+        units: &str,
+    ) -> Result<Stock, &'static str> {
+        Ok(Stock {
+            id: id.to_string(),
+            name: name.to_string(),
+            initial_value,
+            current_value,
+            units: units.to_string(),
+        })
+    }
+}
--- tests/stock_test.rs
+++ tests/stock_test.rs
@@ -1,7 +1,8 @@
 use oxidyn::Stock;
 use std::process;
 
-fn main() {
+#[test]
+fn create_stock() {
     let stock = Stock::build("ID", "NAME", 1.0, 1.0, "UNITS").unwrap_or_else(|err| {
         eprintln!("Problem creating stock `ID`: {err}");
         process::exit(1);
